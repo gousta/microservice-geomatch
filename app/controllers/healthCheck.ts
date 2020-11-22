74b374e093cdbb70fa30916fc629ebd7b02d909e
Stratos Giouldasis <giouldasis.stratos@gmail.com>
2020-11-22 03:30:02 +0200

    Last cleanup, promise

--- app/controllers/healthCheck.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from 'express';
-import config from '../../config';
-
-export function healthCheck(_: Request, res: Response): Response {
-  return res.json({
-    environment: config.environment,
-    uptime: process.uptime()
-  });
-}
--- app/routes.ts
+++ app/routes.ts
@@ -1,9 +1,7 @@
 import { Application } from 'express';
 
-import { healthCheck } from './controllers/healthCheck';
 import { addressSearch } from './controllers/geomatch';
 
 export const init = (app: Application): void => {
-  app.get('/health', healthCheck);
   app.get('/address-search', addressSearch);
 };
